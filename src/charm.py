39ab53e2438a609fa655df0aa7c267e331ff64b4
Dave Wilding <tech@dpw.me>
2026-07-09 09:03:42 +0800

    rename service

--- src/charm.py
+++ src/charm.py
@@ -65,7 +65,7 @@ class FastAPIDemoCharm(ops.CharmBase):
         super().__init__(framework)
         # See 'containers' in charmcraft.yaml.
         self.container = self.unit.get_container("demo-server")
-        self.pebble_service_name = "fastapi-service"
+        self.pebble_service_name = "fastapi"
         framework.observe(self.on["demo-server"].pebble_ready, self._on_demo_server_pebble_ready)
         framework.observe(self.on.config_changed, self._on_config_changed)
         # Report the unit status after each event.
